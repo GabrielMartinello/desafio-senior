bab3b4ee3cf8b07983693b0e495b24c3377e12c9
Gabriel Martinello <martinellogabriel@gmail.com>
2022-11-06 23:18:17 -0300

    Ajuste para criar o banco sozinho

--- src/main/java/com/desafio/senior/desafiosenior/DesafioSeniorApplication.java
+++ src/main/java/com/desafio/senior/desafiosenior/DesafioSeniorApplication.java
@@ -1,12 +1,50 @@
 package com.desafio.senior.desafiosenior;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.*;
 
 @SpringBootApplication
 public class DesafioSeniorApplication {
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(DesafioSeniorApplication.class);
+		Connection connection = null;
+		Statement statement = null;
+		try {
+			logger.debug("Creating database if not exist...");
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "123");
+			statement = connection.createStatement();
+			statement.executeQuery("SELECT count(*) FROM pg_database WHERE datname = 'senior'");
+			ResultSet resultSet = statement.getResultSet();
+			resultSet.next();
+			int count = resultSet.getInt(1);
+
+			if (count <= 0) {
+				statement.executeUpdate("CREATE DATABASE senior");
+				logger.debug("Database created.");
+			} else {
+				logger.debug("Database already exist.");
+			}
+		} catch (SQLException e) {
+			logger.error(e.toString());
+		} finally {
+			try {
+				if (statement != null) {
+					statement.close();
+					logger.debug("Closed Statement.");
+				}
+				if (connection != null) {
+					logger.debug("Closed Connection.");
+					connection.close();
+				}
+			} catch (SQLException e) {
+				logger.error(e.toString());
+			}
+		}
 		SpringApplication.run(DesafioSeniorApplication.class, args);
 	}
 
